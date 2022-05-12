7464b4f5e297c9f58f71870365150942d5d77dd8
Celus8 <celus217@gmail.com>
2022-05-12 14:48:47 +0200

    Add pseudocode for playing a round

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ function computerPlay() {
     } // Assign each number a different play
     return play;
 }
+
+// Create a function that plays a single round of Rock Paper Scissors.
+// Take playerSelection and computerSelection as parameters
+// Create a string variable winner
+// Compare playerSelection to computerSelection and decide winner
+// If player choose rock
+    // If computer choose rock, then tie
+    // If computer choose paper, then assign computer to winner
+    // If computer choose scissors, then assign player to winner
+// If player choose paper
+    // If computer choose rock, then assign player to winner
+    // If computer choose paper, then tie
+    // If computer choose scissors, then assign computer to winner
+// If player choose scissors
+    // If computer choose rock, then assign computer to winner
+    // If computer choose paper, then assign player to winner
+    // If computer choose scissors, then tie
+
+// Return a string that declares the winner of the round and what was played
